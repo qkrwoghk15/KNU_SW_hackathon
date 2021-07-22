842936068e9ac71258104e5db0ffe76a365ad37a
qkrwoghk <akavmvkdldj1@naver.com>
2021-07-22 19:13:38 +0900

    #01.04 <Update Cube Crawling>

--- App/my_pkg/cube_crawling.py
+++ App/my_pkg/cube_crawling.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from datetime import datetime
 import re
+import time
 import pprint
 # ==================================
 # -- Create KNUCUBE notice dict list
@@ -14,22 +15,29 @@ import pprint
 
 now  = datetime.now()
 
-def get_summary(driver, a):
-	driver.execute_script("arguments[0].click();", a)
-	driver.implicitly_wait(3)
-	contents = WebDriverWait(driver, timeout=5).until( #table
-		EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div/div[3]/table/tbody/tr[2]/td'))
-	)
-	print(contents.text)
-	driver.back()
+def get_summary(driver, href):
+	driver.execute_script('window.open("http://knucube.knu.ac.kr/site/reservation/lecture'+href[1:]+'");')
+	driver.implicitly_wait(1)
+	driver.switch_to.window(window_name=driver.window_handles[2])
+	driver.implicitly_wait(1)
 
-	return "0"
+	page = driver.page_source
+	html = BeautifulSoup(page, 'html.parser')
+
+	contents = html.select('.pb_none')[0]
+
+	driver.close()
+	driver.switch_to.window(window_name=driver.window_handles[1])
+
+	return contents.text
 
 def go_next(driver, page):
+	print(f'/html/body/div[1]/div[2]/div/form/ul/li[{str(page+2)}]/a')
 	WebDriverWait(driver, timeout=5).until( #next btn
-		EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div/form/ul/li['+ str(page+2) +']/a'))
+		EC.presence_of_element_located((By.XPATH, f'/html/body/div[1]/div[2]/div/form/ul/li[{str(page+2)}]/a'))
 	).click()
 	return page+1
+	
 
 def get_remainDays(date_to_compare):
 	date_to_compare = datetime.strptime(date_to_compare, "%Y-%m-%d")
@@ -74,41 +82,51 @@ if __name__ == "__main__":
 	#pop up close
 	driver.switch_to.window(window_name=driver.window_handles[-1])
 	driver.close()
+	driver.switch_to.window(window_name=driver.window_handles[-1])
+	main_frame = driver.find_element_by_name("main") #iframe 태그 엘리먼트 찾기
+	driver.switch_to.frame(main_frame) #프레임 이동
 
 	cube_list = []
 	next_page = 2
 	while True:
-		driver.switch_to.window(window_name=driver.window_handles[-1])
-		main_frame = driver.find_element_by_name("main") #iframe 태그 엘리먼트 찾기
-		driver.switch_to.frame(main_frame) #프레임 이동
-
+		page = driver.page_source
+		html = BeautifulSoup(page, 'html.parser')
+		
 		invalid_notice=0
 		
-		tbody = WebDriverWait(driver, timeout=5).until( #table
-			EC.presence_of_element_located((By.CSS_SELECTOR, '#searchForm > div > table > tbody'))
-		)
-		trs = tbody.find_elements_by_tag_name('tr')
+		# tbody = WebDriverWait(driver, timeout=5).until( #table
+		# 	EC.presence_of_element_located((By.CSS_SELECTOR, '#searchForm > div > table > tbody'))
+		# )
+		tbody = html.find('tbody')
+		trs = tbody.find_all('tr')
+		#trs = tbody.find_elements_by_tag_name('tr')
 		for tr in trs:
-			tds = tr.find_elements_by_tag_name('td')
-			if tds[5].text == '신청가능':
-				title = tds[1].text
-				apply_period = tds[2].text.split('\n~ ')
-				summary = get_summary(driver, tds[1].find_element_by_tag_name('a'))
+			#tds = tr.find_elements_by_tag_name('td')
+			tds = tr.find_all('td')
 
+			if tds[5].text.strip() == '신청가능':
+				title = tds[1].text
+				link = tds[1].find('a')['href']
+				apply_period = tds[2].text.split(' ~ ')
+				summary = get_summary(driver, link)
 				cube_dict = {
-					"remainDays": get_remainDays(apply_period[1]),
+					"link": link,
 					"title": title,
+					"remainDays": get_remainDays(apply_period[1]),
 					"startDate": apply_period[0],
 					"endDate": apply_period[1],
 					"summary": summary,
 					#"keywords": "get_keywords(title, summary)"
 				}
+				pprint.pprint(cube_dict)
 				cube_list.append(cube_dict)
 			else:
 				invalid_notice+=1
 			
 		if invalid_notice==10: break
+
 		next_page = go_next(driver, next_page)
+
 
 	# return cube_list
 
